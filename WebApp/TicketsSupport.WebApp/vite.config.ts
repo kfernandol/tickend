5855e60e5aa8ecb0013246148b7aa47b8367e348
Fernando Lima <kaosnox1@hotmail.com>
2024-04-30 16:32:15 -0600

    - Change version project - Change build script use tsconfig.prod.json - Remove prod script

--- WebApp/TicketsSupport.WebApp/vite.config.ts
+++ WebApp/TicketsSupport.WebApp/vite.config.ts
@@ -3,38 +3,10 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 import fs from 'fs';
-import path from 'path';
-import child_process from 'child_process';
 
-const baseFolder =
-    process.env.APPDATA !== undefined && process.env.APPDATA !== ''
-        ? `${process.env.APPDATA}/ASP.NET/https`
-        : `${process.env.HOME}/.aspnet/https`;
-
-const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
-const certificateName = certificateArg ? certificateArg.groups.value : "ticketssupport.client";
-
-if (!certificateName) {
-    console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
-    process.exit(-1);
-}
-
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
+const SSL_PATH = "./ssl/";
+const SSL_KEY_NAME = "cert.key";
+const SSL_CERT_NAME = "cert.pem";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -55,8 +27,8 @@ export default defineConfig({
         },
         port: 5173,
         https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
+            key: fs.readFileSync(SSL_PATH + SSL_KEY_NAME ),
+            cert: fs.readFileSync(SSL_PATH + SSL_CERT_NAME ),
         }
     }
 })
