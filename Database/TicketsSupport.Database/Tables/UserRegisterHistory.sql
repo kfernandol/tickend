03ec3da0b26d73c4ff8943b14faea5e6df8dfe4b
Fernando Lima <kaosnox1@hotmail.com>
2024-05-24 19:56:12 -0600

    - Add table user register history

---
+++ Database/TicketsSupport.Database/Tables/UserRegisterHistory.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[UserRegisterHistory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[UserId] INT FOREIGN KEY REFERENCES [User](Id) NOT NULL,
+	[HashConfirmation] varchar(150) NULL,
+	[IP] varchar(16) NOT NULL,
+	[RegisterDate] DATETIME NOT NULL,
+	[ConfirmationDate] DATETIME NULL,
+	[Confirmed] BIT NOT NULL
+)
